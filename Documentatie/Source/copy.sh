5bf6ab0327574b2861e8dfb3a4fc593fd8bc1e31
Martijn van Dijk <martijn.van.dijk.95@gmail.com>
2015-12-10 17:29:44 +0100

    copy.sh now deletes files with names containing template

--- Documentatie/Source/copy.sh
+++ Documentatie/Source/copy.sh
@@ -3,3 +3,4 @@ mkdir -p ../PDF
 rsync -rv --include '*/' --include '*.pdf' --exclude '*' --prune-empty-dirs ../Source/ ../PDF/
 mv ../PDF/*/*pdf ../PDF/
 find ../PDF/ -type d -empty -delete
+find ../PDF/ -name "*template*.pdf" -delete
