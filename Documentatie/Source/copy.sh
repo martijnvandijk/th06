828b967c5d7dc65bbcb4bfd9a2554ea3b5a0198e
Martijn van Dijk <martijn.van.dijk.95@gmail.com>
2015-12-10 16:45:42 +0100

    updated copy script

--- Documentatie/Source/copy.sh
+++ Documentatie/Source/copy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-mkdir -p PDF
-rsync -rv --include '*/' --include '*.pdf' --exclude '*' --prune-empty-dirs LaTeX/ PDF/ 
+mkdir -p ../PDF
+rsync -rv --include '*/' --include '*.pdf' --exclude '*' --prune-empty-dirs ../Source/ ../PDF/
+mv ../PDF/*/*pdf ../PDF/
+find ../PDF -type d -empty -print0 -exec rm -d "{}" \;
