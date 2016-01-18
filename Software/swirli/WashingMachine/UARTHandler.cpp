21db9678051233537239d1d4766edb18e3615d53
Martijn van Dijk <martijn.van.dijk.95@gmail.com>
2016-01-18 13:52:27 +0100

    reworked UARTHandler

--- Software/swirli/WashingMachine/UARTHandler.cpp
+++ Software/swirli/WashingMachine/UARTHandler.cpp
@@ -10,38 +10,36 @@ namespace WashingMachine {
             serialConnection(serial),
             InputBuffer(this),
             OutputBuffer(),
-            inputQueueCounter{0}
+            timer(this)
             {}
 
     void UARTHandler::sendMessage(UARTMessage m) {
         InputBuffer.write(m);
-        inputQueueCounter++;
         m.sender->suspend();
     }
 
     void UARTHandler::main(){
         while(true){
-//            this->wait(InputBuffer);
-            if(inputQueueCounter){
+            timer.set(10 MS);
+            RTOS::event event = wait(InputBuffer + timer);
+
+            if(event == InputBuffer){
                 UARTMessage message = InputBuffer.read();
                 serialConnection.writeChar(message.requestByte);
                 serialConnection.writeChar(message.commandByte);
-                inputQueueCounter--;
                 OutputBuffer.push_back(message);
             }
-            if(serialConnection.peek() >= 2){
-                uint8_t readBuf[2];
-                serialConnection.read(&readBuf, 2);
-                if((OutputBuffer.front().requestByte | REPLY_BIT ) == readBuf[0])
-                {
-                    OutputBuffer.front().sender->receiveReply(readBuf[1]);
-                    OutputBuffer.front().sender->resume();
-                    OutputBuffer.pop_front();
+            else{
+                if(serialConnection.peek() >= 2 ){
+                    uint8_t readBuf[2];
+                    serialConnection.read(&readBuf, 2);
+                    if((OutputBuffer.front().requestByte | REPLY_BIT) == readBuf[0]){
+                        OutputBuffer.front().sender->receiveReply(readBuf[1]);
+                        OutputBuffer.front().sender->resume();
+                        OutputBuffer.pop_front();
+                    }
                 }
             }
-
-            sleep(10 MS);
-
         }
     }
 }
