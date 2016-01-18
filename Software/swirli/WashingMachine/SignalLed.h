1d4ed39f4119fb6ed61560385b3de2ae1fadbced
Yaluzan <yaluzan@live.nl>
2016-01-18 17:46:19 +0100

    made signal led

--- Software/swirli/WashingMachine/SignalLed.h
+++ Software/swirli/WashingMachine/SignalLed.h
@@ -1,16 +1,19 @@
 #ifndef WASHINGMACHINE_LED_H
 #define WASHINGMACHINE_LED_H
-
+#include "../libs/Protocol.h"
+#include "../libs/libserial.h"
+#include "UARTUser.h"
 #include "UARTHandler.h"
 
 namespace WashingMachine{
+    enum signalled_states_t{LED_ON, LED_OFF};
     class SignalLed{
     public:
         SignalLed(UARTHandler &uart);
-        bool getStatus(UARTUser *referenceUser);
-        void toggle(UARTUser *referenceUser);
-        void enable(UARTUser *referenceUser);
-        void disable(UARTUser *referenceUser);
+
+        signalled_states_t getSignalLedStatus(UARTUser *referenceUser);
+        void set_led(bool status, UARTUser *referenceUser);
+        void toggle_led(UARTUser *referenceUser);
     private:
         UARTHandler &uart;
     };
