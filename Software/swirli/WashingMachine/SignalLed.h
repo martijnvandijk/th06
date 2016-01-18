65b3ff56eac48a6e788db867f4c5c6d5599ba0f6
Martijn van Dijk <martijn.van.dijk.95@gmail.com>
2016-01-18 17:36:24 +0100

    added documentation to signalLED class

--- Software/swirli/WashingMachine/SignalLed.h
+++ Software/swirli/WashingMachine/SignalLed.h
@@ -6,10 +6,42 @@
 namespace WashingMachine{
     class SignalLed{
     public:
+        /**
+         * @brief Constructor, specify UARTHandler
+         * 
+         * @param uart The UARTHandler to be used for communicating with the emulator
+         */
         SignalLed(UARTHandler &uart);
+
+        /**
+         * @brief Get the current status of the status LED
+         * 
+         * @param referenceUser The UARTUser that is calling the function (usually 'this' suffice)
+         * @return true for on, false for off
+         */
         bool getStatus(UARTUser *referenceUser);
+
+        /**
+         * @brief Toggle the status LED status
+         * @details Turns the status LED on when off, and off when on.
+         * 
+         * @param referenceUser The UARTUser that is calling the function (usually 'this' suffice)
+         */
         void toggle(UARTUser *referenceUser);
+        
+        /**
+         * @brief Enable the status LED
+         * 
+         * @param referenceUser The UARTUser that is calling the function (usually 'this' suffice)
+         */
         void enable(UARTUser *referenceUser);
+
+        /**
+         * @brief Disable the status LED
+         * @details [long description]
+         * 
+         * @param referenceUser The UARTUser that is calling the function (usually 'this' suffice)
+         */
         void disable(UARTUser *referenceUser);
     private:
         UARTHandler &uart;
