fcd3a2e1f322f76c2ff30378ccc60ad718e1ad05
Martijn van Dijk <martijn.van.dijk.95@gmail.com>
2016-01-18 17:28:05 +0100

    added documentation to the public members of uarthandler

--- Software/swirli/WashingMachine/UARTHandler.h
+++ Software/swirli/WashingMachine/UARTHandler.h
@@ -13,35 +13,33 @@
 #include "UARTUser.h"
 namespace WashingMachine{
 
+    typedef struct{
+        uint8_t requestByte;
+        uint8_t commandByte;
+        UARTUser *sender;
+    } UARTMessage;
+
     class UARTHandler : public RTOS::task{
     public:
         /**
          * @brief Constructor, specify LibSerial connection
-         * 
-         * @param serial The LibSerial object to use 
+         *
+         * @param serial The LibSerial object to use
          */
         UARTHandler(LibSerial &serial);
 
         /**
          * @brief Send a message to the emulator
-         * 
+         *
          * @param m The UARTMessage to send
          */
         void sendMessage(UARTMessage m);
     private:
         LibSerial &serialConnection;
         RTOS::channel<UARTMessage, 64> InputBuffer;
-        void handleUART();
         void main();
-        int inputQueueCounter;
         RTOS::timer timer;
         std::deque<UARTMessage> OutputBuffer;
     };
-
-    typedef struct{
-        uint8_t requestByte;
-        uint8_t commandByte;
-        UARTUser *sender;
-    } UARTMessage;
 }
 #endif //SWIRLI_UARTHANDLER_H
