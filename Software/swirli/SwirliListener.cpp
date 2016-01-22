43046e385670c5fdd0e9f5f1c12a846ef4256b35
Martijn van Dijk <martijn.van.dijk.95@gmail.com>
2016-01-22 17:07:50 +0100

    removed mutexes

--- Software/swirli/SwirliListener.cpp
+++ Software/swirli/SwirliListener.cpp
@@ -11,12 +11,10 @@ void SwirliListener::onTextMessage(const string &s, WebSocket *ws) {
         std::shared_ptr<WebSocketPacket> webSocketPacket(
                 new WebSocketPacket(s.c_str(), ws)
         );
-//        pQMutex.lock();
         packetQueue.push(webSocketPacket);
         std::cout << "Pushed websocketpacket, queuesize: " << packetQueue.size() << std::endl;
         std::cout << "PacketsAvailable output: " << packetsAvailable() << std::endl;
-//        pQMutex.unlock();
-    }
+\    }
     catch (invalid_argument &e) {
         std::cout << e.what() << std::endl;
     }
@@ -29,26 +27,21 @@ void SwirliListener::onClose(WebSocket *ws) {
 bool SwirliListener::packetsAvailable() {
     bool available;
 
-//    pQMutex.lock();
-
     std::cout << "PacketQueue size: " << packetQueue.size() << std::endl;
     if (packetQueue.size() > 0) {
         available = true;
     } else {
         available = false;
     }
-//    pQMutex.unlock();
     return available;
 }
 
 std::shared_ptr<WebSocketPacket> SwirliListener::getPacket() {
     std::shared_ptr<WebSocketPacket> webSocketPacket;
-//    pQMutex.lock();
 
     webSocketPacket = packetQueue.front();
     std::cout << "popped packet!" << std::endl;
     packetQueue.pop();
-//    pQMutex.unlock();
 
 
     return webSocketPacket;
